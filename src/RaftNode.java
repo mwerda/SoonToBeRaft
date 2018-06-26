8c22701bbba75ac8ff313693025f9cffedee8b80
mwerda <zmalqpzmalqp@gmail.com>
2018-06-26 17:58:02 +0200

    Added javadoc

--- src/RaftNode.java
+++ src/RaftNode.java
@@ -1,3 +1,12 @@
+/**
+ * RaftNode represents a single node in cluster. It orchestrates four distinct threads:
+ * 1. Clock, responsible for signalling timeouts on election and heartbeat and measuring elapsed time
+ * It checks for timeouts every CLOCK_SLEEP_TIME, by default 1 ms
+ * 2. Receiver, doing his job by listening to incoming Drafts, rebuilding them and passing to receivedDrafts
+ * 3. Consumer, deciding on how to process incoming Drafts
+ * 4. Sender, polling Drafts from outgoingDrafts and sending them to other nodes
+ */
+
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.concurrent.*;
@@ -28,7 +37,7 @@ class RaftNode
     final static long[] ELECTION_TIMEOUT_BOUNDS = {150, 300};
     final static long HEARTBEAT_TIMEOUT = 40;
 
-    final static int CLOCK_CHECKING_FREQUENCY = 1;
+    final static int CLOCK_SLEEP_TIME = 1;
 
     RaftNode(byte id, int heartbeatPeriod, int port) throws IOException
     {
@@ -66,6 +75,15 @@ class RaftNode
                     // TODO
                     // handleHeartbeatTimeout()
                     clock.resetHeartbeatTimeoutStartMoment();
+                }
+
+                try
+                {
+                    Thread.sleep(CLOCK_SLEEP_TIME);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
                 }
             }
         });
