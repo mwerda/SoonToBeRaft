f8066e6f1b770783bbb5a5544a5bdee075f5f638
mwerda <zmalqpzmalqp@gmail.com>
2018-07-01 21:40:16 +0200

    ClientSession reading Drafts

--- src/networking/ClientSession.java
+++ src/networking/ClientSession.java
@@ -1,5 +1,9 @@
 package networking;
 
+// Questions to answer:
+// Should non-blocking read be more effective?
+// WHat is the thrpughput of current solution?
+
 import jdk.nashorn.internal.ir.Block;
 
 import java.io.IOException;
@@ -22,17 +26,21 @@ public class ClientSession
         this.channel = channel;
         this.channel.configureBlocking(true);
         this.buffer = ByteBuffer.allocateDirect(bufferSize);
+        this.buffer.clear();
         this.draftQueue = new LinkedBlockingQueue<>();
     }
 
     public void readDraft() throws IOException
     {
         channel.read(buffer);
+        buffer.flip();
         int draftLength = buffer.getInt();
+        buffer.position(0);
         if(buffer.limit() >= draftLength - 1)
         {
             byte[] draftBytes = new byte[draftLength];
-            draftQueue.add(Draft.fromByteArray(buffer.get(draftBytes).array()));
+            buffer.get(draftBytes);
+            draftQueue.add(Draft.fromByteArray(draftBytes));
         }
     }
 }
